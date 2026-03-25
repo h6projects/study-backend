8d9c7b5bdcbae95e5c933a6547c399c0fae83258
HG <hg@HG-MacBook-Air.local>
2026-03-25 00:36:04 +0000

    use PORT env var with fallback to 5001

--- app.py
+++ app.py
@@ -361,4 +361,5 @@ def _safe_key(key):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    port = int(os.getenv("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
